f5084bf91bd1450d98cf5c2ed8bdfeaa3233db99
jinjini000 <ggguree@gmail.com>
2025-12-05 23:50:15 +0900

    첫 커밋

---
+++ server.mjs
@@ -0,0 +1,53 @@
+import express from "express";
+import path from "path";
+import { createServer } from "http";
+import fs from "fs";
+import { Server } from "socket.io";
+import { fileURLToPath } from "url";
+
+// 기본 묶음
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+let offer = {};
+let answer = {};
+
+io.on("connection", (socket) => {
+  console.log("웹소켓 연결완료");
+
+  // 4. 오퍼를 받고 서버에 저장
+  socket.on("sendOfferToServer", (sentOffer) => {
+    console.log("오퍼받았음");
+    offer = sentOffer;
+    console.log(offer);
+  });
+
+  // 6. 사용자의 요청을 받고 저장된 오퍼를 반환
+  socket.on("getOfferFromServer", () => {
+    socket.emit("sendOfferFromServer", offer);
+    console.log("사용자에게 오퍼 보냈음");
+  });
+
+  // 12. answer를 서버에 저장
+  socket.on("sendAnswerToServer", (sentanswer) => {
+    console.log(sentanswer);
+    answer = sentanswer;
+    // broadcast: Answer를 보낸 이용자를 제외한 모든 클라이언트에게 전송
+    // 13. answer를 반환
+    socket.broadcast.emit("sendAnswerFromServer", answer);
+  });
+
+  // 16. 받은 ice를 뿌려줌
+  socket.on("iceCandidate", (ice) => {
+    socket.broadcast.emit("iceCandidateFromServer", ice);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("서버 실행중");
+});
