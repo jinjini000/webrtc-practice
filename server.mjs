130b44c3e7e8ce5668c76f092718538931ffb6ff
jinjini000 <jinjini000@naver.com>
2025-12-09 15:31:52 +0900

    방까지 만들었음! 예외처리 아직 안됐음!

--- server.mjs
+++ server.mjs
@@ -11,6 +11,7 @@ const io = new Server(server);
 let pendingCandidates = [];
 
 let offer = null;
+let roomlist = [];
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -20,10 +21,13 @@ app.use(express.static(path.join(__dirname, "public")));
 
 io.on("connection", (socket) => {
   console.log("WebSocket 연결됨:", socket.id);
+  socket.emit("roomlist", roomlist);
 
   // Caller → Server → Remote 로 Offer 전달
-  socket.on("offer", (sentoffer) => {
+  socket.on("offer", (sentoffer, room) => {
     console.log("Offer 수신, 브로드캐스트");
+    roomlist.push(room);
+    console.log(roomlist);
     offer = sentoffer;
   });
 
@@ -42,17 +46,25 @@ io.on("connection", (socket) => {
   });
 
   // 양쪽 ICE 후보 교환 (그냥 상대에게 브로드캐스트)
-  socket.on("ice-candidate", (candidate, isCaller) => {
+  socket.on("ice-candidate", (candidate, isCaller, room) => {
     console.log("ICE Candidate 수신, 브로드캐스트");
     if (isCaller) {
-      pendingCandidates.push(candidate);
+      pendingCandidates.push({
+        room: room,
+        candidate: candidate,
+      });
     }
+    console.log(pendingCandidates);
     socket.broadcast.emit("send-ice-candidate", candidate);
   });
 
-  socket.on("getIceOfCaller", () => {
-    while (pendingCandidates.length > 0) {
-      const c = pendingCandidates.shift();
+  socket.on("getIceOfCaller", (room) => {
+    const filtered = pendingCandidates.filter((item) => item.room === room);
+    const list = filtered
+      .filter((item) => item.room === room) // room 기준 필터링
+      .map((item) => item.candidate);
+    while (list.length > 0) {
+      const c = list.shift();
       try {
         socket.emit("sendIceOfCaller", c);
         console.log("대기중인 ICE 후보 추가 처리");
@@ -60,6 +72,13 @@ io.on("connection", (socket) => {
         console.error("대기중인 ICE 후보 추가 중 에러:", e);
       }
     }
+  });
+
+  socket.on("DataChannel closed", (room) => {
+    console.log(room);
+    roomlist = roomlist.filter((item) => item !== room);
+    socket.broadcast.emit("roomlist", roomlist);
+    socket.broadcast.emit("refresh");
   });
 });
 
